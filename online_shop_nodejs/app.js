9a3853f620e0d02a67c4e1900942a190e0aeddbf
leviettien1808 <leviettien1808@gmail.com>
2025-07-28 18:34:47 +0700

    Handlebars template

--- online_shop_nodejs/app.js
+++ online_shop_nodejs/app.js
@@ -2,8 +2,20 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const { engine } = require("express-handlebars");
 
 const app = express();
+
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -15,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
