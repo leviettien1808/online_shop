86da46a5c42d971e720cea83943118a9ba9bb4bf
leviettien1808 <leviettien1808@gmail.com>
2025-09-30 20:08:25 +0700

    [Server_Models] Connect to database

--- online_shop_nodejs/models/product.js
+++ online_shop_nodejs/models/product.js
@@ -1,19 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-
-const p = path.join(
-  path.dirname(require.main.filename),
-  "data",
-  "products.json"
-);
-const getProductsFromFile = (cb) => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    }
-    cb(JSON.parse(fileContent));
-  });
-};
+const db = require("../util/database");
 
 module.exports = class Product {
   constructor(title, imageUrl, description, price) {
@@ -23,16 +8,13 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-    getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
+  save() {}
+
+  deleteById(id) {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+  static findById(id) {}
 };
